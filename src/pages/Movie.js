9c7abf1671e2ad916500018b79df3e20635e5c05
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-05-28 21:03:02 -0400

    Router Lab

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import React from "react";
 
 function Movie() {
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(r => r.json())
+      .then(setMovie)
+      .catch(error => console.error(error));
+  }, [id]);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <span>{movie.genres}</span>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
+
